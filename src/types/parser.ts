8e0d25855b22dfade284c11a33cb89118865abb9
Evanzyli <656603136@qq.com>
2021-06-01 16:51:43 +0800

    fixed: init

--- src/types/parser.ts
+++ src/types/parser.ts
@@ -1,10 +1,62 @@
 export interface Parse {
-    filePath: string
+  filePath: string
 }
 export interface ComponentDoc {
-    displayName: string;
+  displayName: string
+  description: string
+  props: Props
+  methods: Method[]
+  tags?: {}
+}
+
+export interface MethodParameterType {
+    name: string;
+  }
+export interface MethodParameter {
+    name: string;
+    description?: string | null;
+    type: MethodParameterType;
+  }
+  
+export interface Method {
+    name: string;
+    docblock: string;
+    modifiers: string[];
+    params: MethodParameter[];
+    returns?: {
+      description?: string | null;
+      type?: string;
+    } | null;
     description: string;
-    props: Props;
-    methods: Method[];
+  }
+
+  
+export interface PropItemType {
+    name: string;
+    value?: any;
+    raw?: string;
+  }
+  
+export interface StringIndexedObject<T> {
+    [key: string]: T;
+  }
+export interface Props extends StringIndexedObject<PropItem> {}
+
+export interface ParentType {
+    name: string;
+    fileName: string;
+  }
+
+  
+export interface PropItem {
+    name: string;
+    required: boolean;
+    type: PropItemType;
+    description: string;
+    defaultValue: any;
+    parent?: ParentType;
+    declarations?: ParentType[];
     tags?: {};
   }
+
+  
